05bcb5d3650e437df4cbe7e0188bd5af529f3e24
Nkateko Makukule <makukulenkateko@yahoo.com>
2023-02-22 13:22:39 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h)
+
+/**
+ * _abs - compute the absolute value of an interger
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		retun (n);
+	}
+	return (-n);
+}
+	
