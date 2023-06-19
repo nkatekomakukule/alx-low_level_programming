e00c947250f9eb97be1b1923f16970ed679b5e44
nkatekomakukule <makukulenkateko@gmail.com>
2023-06-19 10:18:46 -0700

    correction

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,9 +4,9 @@
 /**
  * init_dog - initializes a variable of type struct dog
  * @d: pointer to struct dog to initialize
- * @name: Fluffy
- * @age: 2 
- * @owner: Nkateko
+ * @name: Fluffy to initialize
+ * @age: 2 to initialize
+ * @owner: Nkateko to initialize
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
